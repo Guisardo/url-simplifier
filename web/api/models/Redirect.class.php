391c4fd4dc4462f7be39d845b8e461345dbe6456
Lucas Rancez <lucas.rancez@fox.com>
2016-03-30 23:48:55 -0300

    added verbose to hit

--- web/api/models/Redirect.class.php
+++ web/api/models/Redirect.class.php
@@ -120,6 +120,7 @@ class Redirect {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_VERBOSE, true);
             $data = curl_exec($ch);
             curl_close($ch);
         }
